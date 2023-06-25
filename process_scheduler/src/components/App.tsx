48fb7af38c9481b026b1f17d0b99cba2be9fcab8
Tauane Sales <taysales6@gmail.com>
2023-06-24 23:32:34 -0300

    [ADD/UPT] Started front gantt-chart component.

--- process_scheduler/src/components/App.tsx
+++ process_scheduler/src/components/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "../assets/img/logo.png";
 import InputsAndMethods from "./InputsAndMethods";
 import CreateProcesses from "./CreateProcesses";
+import FrontGanttChart from "./FrontGanttChart";
 import "./App.css";
 
 // Define the App component
@@ -11,6 +12,7 @@ const App: React.FC = () => {
       <img src={logo} alt="Logo" className="logo" />
       <InputsAndMethods />
       <CreateProcesses />
+      <FrontGanttChart />
     </div>
   );
 };
---
+++ process_scheduler/src/components/FrontGanttChart.tsx
@@ -0,0 +1,5 @@
+const FrontGanttChart: any = () => {
+  return <div className="box yellow"></div>;
+};
+
+export default FrontGanttChart;
