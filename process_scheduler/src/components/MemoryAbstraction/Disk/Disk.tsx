af1ab925f7b82ea8a306b72d8b513284f390008f
Gustavo Jorge <gustavojorge080@gmail.com>
2023-07-05 00:37:40 -0300

    Reset bug resolved

--- process_scheduler/src/components/MemoryAbstraction/Disk/Disk.tsx
+++ process_scheduler/src/components/MemoryAbstraction/Disk/Disk.tsx
@@ -1,73 +1,83 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./Disk.css";
 import PaginationData from "../../../interfaces/PaginationData";
 
-interface DiskProps{
-  pagingData: PaginationData[],
-  intervalo: number,
-  play : boolean,
-  reset: boolean
+interface DiskProps {
+  pagingData: PaginationData[];
+  intervalo: number;
+  play: boolean;
+  reset: boolean;
 }
-const DiskMemory: React.FC<DiskProps> = ({pagingData, intervalo, play, reset})=> {
 
-
-  const [matrix, setMatrix] = useState<({ value: number | string, address: number } | string | number)[][]>(
-    Array.from({ length: 10 }, () => Array(12).fill("-"))
-  );
+const DiskMemory: React.FC<DiskProps> = ({
+  pagingData,
+  intervalo,
+  play,
+  reset,
+}) => {
+  const [matrix, setMatrix] = useState<
+    ({ value: number | string; address: number } | string | number)[][]
+  >(Array.from({ length: 10 }, () => Array(12).fill("-")));
   const [currentStep, setCurrentStep] = useState(1);
-    
-    useEffect(() => {
-      const newMatrix = matrix.map((row) => [...row]);
-      for (let i = 0; i < 120; i++) {
-        const colIndex = i % 12;
-        const rowIndex = Math.floor(i / 12);
-        newMatrix[rowIndex][colIndex] = "-";
-      }
-      setMatrix(newMatrix);
-      
-    }, [reset]);
 
+  const isResetting = useRef(false); // Referência para controlar o reset
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (currentStep < pagingData.length) {
-        const currentDisco = pagingData[currentStep].disk;
-        const newMatrix = matrix.map((row) => [...row]);  // Copia da matriz
+    if (reset && !isResetting.current) {
+      // Verifica se é um novo reset e executa apenas uma vez
+      isResetting.current = true;
 
-        for (let i = 0; i < currentDisco.length; i++) {
-          const value = currentDisco[i];
-          const colIndex = i % 12; // Cálculo do índice da coluna
-          const rowIndex = Math.floor(i / 12); // Cálculo do índice da linha
-          const address = rowIndex * 12 + colIndex; // Cálculo do endereço
-          newMatrix[rowIndex][colIndex] = { value, address };
+      const newMatrix = Array.from({ length: 10 }, () => Array(12).fill("-"));
+      setMatrix(newMatrix);
+      setCurrentStep(1);
+
+      // Define um intervalo de 1 segundo para desabilitar o estado de reset
+      setTimeout(() => {
+        isResetting.current = false;
+      }, 1000);
+    }
+  }, [reset]);
+
+  useEffect(() => {
+    if (!isResetting.current) {
+      const interval = setInterval(() => {
+        if (currentStep < pagingData.length) {
+          const currentDisco = pagingData[currentStep].disk;
+          const newMatrix = matrix.map((row) => [...row]);
+
+          for (let i = 0; i < currentDisco.length; i++) {
+            const value = currentDisco[i];
+            const colIndex = i % 12;
+            const rowIndex = Math.floor(i / 12);
+            const address = rowIndex * 12 + colIndex;
+            newMatrix[rowIndex][colIndex] = { value, address };
+          }
+
+          for (let i = currentDisco.length; i < 120; i++) {
+            const colIndex = i % 12;
+            const rowIndex = Math.floor(i / 12);
+            newMatrix[rowIndex][colIndex] = "-";
+          }
+
+          setMatrix(newMatrix);
+          setCurrentStep((prevStep) => prevStep + 1);
+        } else {
+          clearInterval(interval);
         }
+      }, intervalo);
 
-        // Preenche os elementos restantes com "-"
-        for (let i = currentDisco.length; i < 120; i++) {
-          const colIndex = i % 12;
-          const rowIndex = Math.floor(i / 12);
-          newMatrix[rowIndex][colIndex] = "-";
-        }
-
-        setMatrix(newMatrix);
-        setCurrentStep((prevStep) => prevStep + 1);
-      } else {
+      return () => {
         clearInterval(interval);
-      }
-    }, intervalo);
-
-    return () => {
-      clearInterval(interval);
-    };
+      };
+    }
   }, [currentStep, matrix, play]);
-
 
   return (
     <div className="matrix-container-disk">
       <h1>Disco</h1>
-      {matrix.map((row: (number | string | { value: number | string, address: number })[], rowIndex: number) => (
+      {matrix.map((row: (number | string | { value: number | string; address: number })[], rowIndex: number) => (
         <div key={rowIndex} className="matrix-row-disk">
-          {row.map((cell: number | string | { value: number | string, address: number }, cellIndex: number) => (
+          {row.map((cell: number | string | { value: number | string; address: number }, cellIndex: number) => (
             <div key={cellIndex} className="matrix-cell-disk">
               {typeof cell === "object" ? (
                 <div>
@@ -83,6 +93,6 @@ const DiskMemory: React.FC<DiskProps> = ({pagingData, intervalo, play, reset})=>
       ))}
     </div>
   );
-}
+};
 
 export default DiskMemory;
--- process_scheduler/src/components/MemoryAbstraction/RAM/MainMemory.tsx
+++ process_scheduler/src/components/MemoryAbstraction/RAM/MainMemory.tsx
@@ -1,81 +1,86 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./MainMemory.css";
 import PaginationData from "../../../interfaces/PaginationData";
 
-interface MainMemoryProps{
-  pagingData: PaginationData[],
-  intervalo: number,
-  play : boolean,
-  reset: boolean
+interface MainMemoryProps {
+  pagingData: PaginationData[];
+  intervalo: number;
+  play: boolean;
+  reset: boolean;
 }
-const MainMemory: React.FC<MainMemoryProps> = ({pagingData, intervalo, play, reset})=> {
 
-  // const pagingData = [
-  //   { step: 0, ram: [242, 1, 1, 1, NaN, 242, 1, 1, 1, NaN, 242, 1, 1, 1, NaN, 242, 1, 1, 1, NaN, 242, 1, 1, 1, NaN, 242, 1, 1, 1, NaN, 242, 1, 1, 1, NaN, 242, 1, 1, 1, NaN, 242, 1, 1, 1, NaN, 242, 1, 1, 1, NaN], disco: [9, 9, 9, 9] },
-  //   { step: 1, ram: [2, 2, 1, 1, 3], disco: [8, 8, 8, 8] },
-  //   { step: 2, ram: [4, 5, 1, 1, 4], disco: [7, 7, 7, 7] }
-  // ];
-
-  const [matrix, setMatrix] = useState<({ value: number | string, address: number } | string | number)[][]>(
-    Array.from({ length: 5 }, () => Array(10).fill("-"))
-  );
+const MainMemory: React.FC<MainMemoryProps> = ({
+  pagingData,
+  intervalo,
+  play,
+  reset,
+}) => {
+  const [matrix, setMatrix] = useState<
+    ({ value: number | string; address: number } | string | number)[][]
+  >(Array.from({ length: 5 }, () => Array(10).fill("-")));
   const [currentStep, setCurrentStep] = useState(1);
-  
+
+  const isResetting = useRef(false);
+
   useEffect(() => {
-    const newMatrix = matrix.map((row) => [...row]);
-    for (let i = 0; i < 50; i++) {
-      const colIndex = i % 10;
-      const rowIndex = Math.floor(i / 10);
-      newMatrix[rowIndex][colIndex] = "-";
+    if (reset && !isResetting.current) {
+      isResetting.current = true;
+
+      const newMatrix = Array.from({ length: 5 }, () => Array(10).fill("-"));
+      setMatrix(newMatrix);
+      setCurrentStep(1);
+
+      setTimeout(() => {
+        isResetting.current = false;
+      }, 1000);
     }
-    setMatrix(newMatrix);
-    setCurrentStep(1);
-  }, [reset])
+  }, [reset]);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (currentStep < pagingData.length) {
-        const currentRam = pagingData[currentStep].ram;
-        const newMatrix = matrix.map((row) => [...row]); // Copia da matriz
+    if (!isResetting.current) {
+      const interval = setInterval(() => {
+        if (currentStep < pagingData.length) {
+          const currentRam = pagingData[currentStep].ram;
+          const newMatrix = matrix.map((row) => [...row]);
 
-        for (let i = 0; i < currentRam.length; i++) {
-          const value = currentRam[i];
-          const rowIndex = Math.floor(i / 10); // Cálculo do index da linha
-          const colIndex = i % 10; // Cálculo do deslocamento dentro da linha
-          const address = rowIndex * 10 + colIndex; // Cálculo do endereço
-          newMatrix[rowIndex][colIndex] = { value, address };
+          for (let i = 0; i < currentRam.length; i++) {
+            const value = currentRam[i];
+            const rowIndex = Math.floor(i / 10);
+            const colIndex = i % 10;
+            const address = rowIndex * 10 + colIndex;
+            newMatrix[rowIndex][colIndex] = { value, address };
+          }
+
+          for (let i = currentRam.length; i < 50; i++) {
+            const rowIndex = Math.floor(i / 10);
+            const colIndex = i % 10;
+            newMatrix[rowIndex][colIndex] = "-";
+          }
+
+          setMatrix(newMatrix);
+          setCurrentStep((prevStep) => prevStep + 1);
+        } else {
+          clearInterval(interval);
         }
+      }, intervalo);
 
-        // Preenche os elementos restantes com "-"
-        for (let i = currentRam.length; i < 50; i++) {
-          const rowIndex = Math.floor(i / 10);
-          const colIndex = i % 10;
-          newMatrix[rowIndex][colIndex] = "-";
-        }
-
-        setMatrix(newMatrix);
-        setCurrentStep((prevStep) => prevStep + 1);
-      } else {
+      return () => {
         clearInterval(interval);
-      }
-    }, intervalo);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [ currentStep, matrix, play]);
+      };
+    }
+  }, [currentStep, matrix, play, isResetting]);
 
   return (
     <div className="matrix-container">
       <h1>RAM</h1>
-      {matrix.map((row: (number | string | { value: number | string, address: number })[], rowIndex: number) => (
+      {matrix.map((row: (number | string | { value: number | string; address: number })[], rowIndex: number) => (
         <div key={rowIndex} className="matrix-row">
-          {row.map((cell: number | string | { value: number | string, address: number }, cellIndex: number) => (
+          {row.map((cell: number | string | { value: number | string; address: number }, cellIndex: number) => (
             <div key={cellIndex} className="matrix-cell">
               {typeof cell === "object" ? (
                 <div>
                   <div className="cell-address">{cell.address}</div>
-                  <div className="cell-value">{cell.value}</div>                  
+                  <div className="cell-value">{cell.value}</div>
                 </div>
               ) : (
                 cell
@@ -86,6 +91,6 @@ const MainMemory: React.FC<MainMemoryProps> = ({pagingData, intervalo, play, res
       ))}
     </div>
   );
-}
+};
 
 export default MainMemory;
